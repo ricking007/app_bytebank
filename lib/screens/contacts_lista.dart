d9988b9161d4ffcdea4f427c804f0ea94f423e48
ricardo <ricardo.oliveira@infocraft.com.br>
2020-08-12 15:52:43 -0300

    Listando os contatos salvos no banco de dados

--- lib/screens/contacts_lista.dart
+++ lib/screens/contacts_lista.dart
@@ -1,9 +1,9 @@
+import 'package:app_bytebank/database/app_database.dart';
 import 'package:app_bytebank/models/contact.dart';
 import 'package:app_bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-
   final List<Contact> contacts = List();
 
   @override
@@ -12,25 +12,30 @@ class ContactsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Contact
-          contact = contacts[index];
-          return _ContactItem(contact);
+      body: FutureBuilder(
+        future: findAll(),
+        builder: (context, snapshot) {
+          final List<Contact> contacts = snapshot.data;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final Contact contact = contacts[index];
+              return _ContactItem(contact);
+            },
+            itemCount: contacts.length,
+          );
         },
-        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
               .push(
-            MaterialPageRoute(
-              builder: (context) => ContactForm(),
-            ),
-          )
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
               .then(
                 (newContact) => debugPrint(newContact.toString()),
-          );
+              );
         },
         child: Icon(Icons.add),
       ),
@@ -39,7 +44,6 @@ class ContactsList extends StatelessWidget {
 }
 
 class _ContactItem extends StatelessWidget {
-
   final Contact contact;
 
   _ContactItem(this.contact);
@@ -50,11 +54,15 @@ class _ContactItem extends StatelessWidget {
       child: ListTile(
         title: Text(
           contact.name,
-          style: TextStyle(fontSize: 24.0,),
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
         ),
         subtitle: Text(
           contact.accountNumber.toString(),
-          style: TextStyle(fontSize: 16.0,),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
